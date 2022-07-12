49d012b5a11da48b061ba24233617e03e5cb7ae4
Vico Zhang <1248130720@qq.com>
2022-07-12 19:43:44 +0800

    上传仿真文件

---
+++ Simulation/Project_220704.gf42/Project_220704.30065.bat
@@ -0,0 +1,4 @@
+call "E:\Program Files (x86)\GFortran\4.2.1\bin\gf42vars.bat"
+pushd "D:\SRP_project\Project_0704\Simulation\Project_220704.gf42\"
+Project_220704.exe -v4 192.168.31.62 30065 locale=english-us
+popd
---
+++ Simulation/Project_220704.gf42/Project_220704.30129.bat
@@ -0,0 +1,4 @@
+call "E:\Program Files (x86)\GFortran\4.2.1\bin\gf42vars.bat"
+pushd "D:\SRP_project\Project_0704\Project_220704.gf42\"
+Project_220704.exe -v4 127.0.0.1 30129 locale=english-us
+popd
