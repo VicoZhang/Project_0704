43bded24563e2ec81c6ee069288e70c56e57745d
Vico Zhang <1248130720@qq.com>
2022-07-15 17:16:07 +0800

    完善了保存机制

--- Net/CNN.py
+++ Net/CNN.py
@@ -5,6 +5,7 @@ from torch.utils import tensorboard
 from torch import nn, optim
 from datetime import datetime
 import torch
+import os
 
 
 train_data_loader = DataLoader(Data_Set.train_dataset, batch_size=4, shuffle=True)
@@ -19,13 +20,15 @@ train_step = 0
 test_step = 0
 test_loss = 0
 time = "{0:%Y_%m_%dT%H_%M_%S}".format(datetime.now())
+tensorboard_path = 'logs_1wave/{}/'.format(time)
+net_save_path = '../Result_1wave/Net_result_{}.pth'.format(time)
 
 net = Net.Net()
 net = net.cuda()
 loss_fn = nn.CrossEntropyLoss()
 loss_fn = loss_fn.cuda()
 optimizer = optim.SGD(net.parameters(), learn_rate)
-writer = tensorboard.SummaryWriter('logs/{}/'.format(time))
+writer = tensorboard.SummaryWriter(log_dir=tensorboard_path)
 
 for epoch in range(epochs):
     print("第{}轮训练开始".format(epoch+1))
@@ -57,3 +60,18 @@ for epoch in range(epochs):
         print("第{}次测试完成, 测试集损失函数值{}".format(test_step, test_loss))
 
 writer.close()
+torch.save(net.state_dict(), f=net_save_path)
+print("模型已保存")
+with open(os.path.join(tensorboard_path, 'information'), 'w') as f:
+    f.write('epochs = {}}\n'
+            'learn_rate = {}}\n'
+            'loss_fn = {}}\n'
+            'optimizer = {}}\n'
+            'len(Data_Set.train_dataset)={}\n'
+            'len(Data_Set.test_dataset)={}\n'.format(epochs,
+                                                     learn_rate,
+                                                     loss_fn,
+                                                     optimizer,
+                                                     len(Data_Set.train_dataset),
+                                                     len(Data_Set.test_dataset)))
+print("模型信息已保存")
